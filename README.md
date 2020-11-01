b67b1c51d85ec19478bfde7e9b8688d8545006da
Neil Ricci <iccir919@gmail.com>
2020-10-31 18:24:00 -0700

    Added chart for daily total number of trips

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Bay Wheels 2020 Data Analysis
 
+## Bay Wheels API Feeds
+ - {"name":"system_information","url":"https://gbfs.baywheels.com/gbfs/en/system_information.json"}
+ - "name":"station_information","url":"https://gbfs.baywheels.com/gbfs/en/station_information.json"}
+ - {"name":"station_status","url":"https://gbfs.baywheels.com/gbfs/en/station_status.json"}
+ - {"name":"free_bike_status","url":"https://gbfs.baywheels.com/gbfs/en/free_bike_status.json"}
+ - {"name":"system_hours","url":"https://gbfs.baywheels.com/gbfs/en/system_hours.json"}
+ - {"name":"system_calendar","url":"https://gbfs.baywheels.com/gbfs/en/system_calendar.json"}
+ - {"name":"system_regions","url":"https://gbfs.baywheels.com/gbfs/en/system_regions.json"}
+ - {"name":"system_alerts","url":"https://gbfs.baywheels.com/gbfs/en/system_alerts.json"}]
+
+
 ## Helpful Resources
 - [How to import a CSV file into a MySQL database?](https://medium.com/@AviGoom/how-to-import-a-csv-file-into-a-mysql-database-ef8860878a68)
 - [ERROR: Loading local data is disabled - this must be enabled on both the client and server sides](https://stackoverflow.com/questions/59993844/error-loading-local-data-is-disabled-this-must-be-enabled-on-both-the-client)
--- analysis/analysis_queries.sql
+++ analysis/analysis_queries.sql
@@ -6,26 +6,61 @@ GROUP BY monthname(started_at), rideable_type
 ORDER BY total DESC;
 
 /* Total number of departures and arrivals by docking station */
-SELECT station_to_station_trips.station_id AS station_id, COUNT(*) AS total
+SELECT 
+    station_activity_by_id.station_id AS station_id, 
+    station_info.station_name,
+    station_info.station_lat,
+    station_info.station_lng,
+    COUNT(*) AS total
 FROM
 (
 SELECT start_station_id AS station_id FROM bay_wheels_2020 WHERE start_station_id != 0
 UNION ALL
 SELECT end_station_id AS station_id FROM bay_wheels_2020 WHERE end_station_id != 0
-) AS station_to_station_trips
-GROUP BY station_to_station_trips.station_id
-ORDER BY station_to_station_trips.station_id;
+) AS station_activity_by_id
+JOIN (
+    SELECT 
+        end_station_id,
+        end_station_name AS station_name,
+        end_lat AS station_lat,
+        end_lng AS station_lng
+    FROM
+        bay_wheels_2020
+    WHERE end_station_id != 0
+    GROUP BY end_station_id
+) AS station_info 
+ON station_info.end_station_id = station_activity_by_id.station_id
+GROUP BY station_activity_by_id.station_id
+ORDER BY station_activity_by_id.station_id;
 
 /* Total number of departures by station */
-SELECT start_station_id AS station_id, COUNT(*) AS total
+SELECT 
+    start_station_id AS station_id, 
+    start_station_name AS station_name,
+    start_lat AS station_lat,
+    start_lng AS station_lng,
+    COUNT(*) AS total
 FROM bay_wheels_2020 
 WHERE start_station_id != 0
 GROUP BY station_id
 ORDER BY station_id;
 
 /* Total number of arrivals by station */
-SELECT end_station_id AS station_id, COUNT(*) AS total
+SELECT 
+    end_station_id AS station_id, 
+    end_station_name AS station_name,
+    end_lat AS station_lat,
+    end_lng AS station_lng,
+    COUNT(*) AS total
 FROM bay_wheels_2020 
 WHERE end_station_id != 0
 GROUP BY station_id
 ORDER BY station_id;
+
+/* Total number of trips per day */
+SELECT 
+    DATE_FORMAT(started_at, '%Y-%m-%d') AS day,
+    COUNT(*) AS total
+FROM bay_wheels_2020
+GROUP BY day
+ORDER BY day;
